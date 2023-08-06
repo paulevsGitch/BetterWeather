1c00a23c8cf84cd445fa7342396e6b258663e404
paulevsGitch <paulevs@yandex.ru>
2023-08-06 15:58:11 +0300

    Gradle properties change

--- src/main/java/paulevs/betterweather/mixin/common/LevelMixin.java
+++ src/main/java/paulevs/betterweather/mixin/common/LevelMixin.java
@@ -23,35 +23,4 @@ public class LevelMixin {
 	private void betterweather_isRainingAt(int x, int y, int z, CallbackInfoReturnable<Boolean> info)  {
 		info.setReturnValue(WeatherAPI.isRaining(Level.class.cast(this), x, y, z));
 	}
-	
-	/*@Inject(method = "getRainGradient", at = @At("HEAD"), cancellable = true)
-	private void betterweather_getRainGradient(float delta, CallbackInfoReturnable<Float> info) {
-		if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
-			@SuppressWarnings("deprecation")
-			Minecraft minecraft = (Minecraft) FabricLoader.getInstance().getGameInstance();
-			if (minecraft.viewEntity != null && minecraft.level != null) {
-				float value = WeatherAPI.isRaining(
-					minecraft.level,
-					MathHelper.floor(minecraft.viewEntity.x),
-					MathHelper.floor(minecraft.viewEntity.y),
-					MathHelper.floor(minecraft.viewEntity.z)
-				) ? 1F : 0F;
-				info.setReturnValue(value);
-			}
-		}
-		info.setReturnValue(0F);
-	}*/
-	
-	/*@ModifyVariable(
-		method = "getSkyColor(Lnet/minecraft/entity/BaseEntity;F)Lnet/minecraft/util/maths/Vec3f;",
-		at = @At(value = "STORE"), ordinal = 12, argsOnly = true
-	)
-	private float betterweather_changeSkyColor(float x) {
-		return 1F;
-	}*/
-	
-	/*@Inject(method = "getSkyColor", at = )
-	private void betterweather_changeSkyColor() {
-	
-	}*/
 }
